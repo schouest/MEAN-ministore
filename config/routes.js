4288e78e07472e57719cb575a64cca5cbd3d36ba
Stephen <e2stephen@cox.net>
2015-07-01 13:48:07 -0700

    initial commit

---
+++ config/routes.js
@@ -0,0 +1,19 @@
+// First at the top of your routes.js file you'll have to require the controller
+//var friends = require('./../server/controllers/friends.js');
+
+// This is our routes.js file located in /config/routes.js
+  // We will have to require this in the server.js file and pass it app
+  module.exports = (function(app) {
+    app.get('/friends', function(req, res) {
+    	friends.show(req, res);
+    });
+
+    app.post('/add', function(req, res) {
+    	friends.add(req, res);
+    });
+
+    app.get('/delete/:id', function(req, res) {
+    	friends.remove(req, res);
+    });
+
+  });
